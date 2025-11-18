a620df856a6cebfaf8857ae11925117eab378525
joaoemanuels <jemanuel.pi@gmail.com>
2025-11-18 11:58:47 -0300

    ajustando o addTarefa

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,18 @@
 const btnAdcionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
+const textarea = document.querySelector(".app__form-textarea");
+
+const tarefas = [];
 
 btnAdcionarTarefa.addEventListener("click", () => {
-  formAdicionarTarefa.classList.toggle("hidden")
+  formAdicionarTarefa.classList.toggle("hidden");
+});
+
+formAdicionarTarefa.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  const tarefa = {
+    descricao: textarea.value,
+  };
+  tarefas.push(tarefa);
+  localStorage.setItem("tarefas", tarefas);
 });
