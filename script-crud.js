e94cb55d398a3ebad71bc703064bbc659c89c410
joaoemanuels <jemanuel.pi@gmail.com>
2025-12-03 11:42:44 -0300

    ajustes

--- script-crud.js
+++ script-crud.js
@@ -1,18 +1,61 @@
 const btnAdcionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
-const textarea = document.querySelector(".app__form-textarea");
+const btnAddTask = document.querySelector(".btn-add-task");
+const formAddTask = document.querySelector(".form-add-task");
+const ulTarefas = document.querySelector(".app__section-task-list");
 
-const tarefas = [];
+const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+function criarElementoTarefa(tarefa) {
+  const li = document.createElement("li");
+  li.classList.add("app__section-task-list-item");
+
+  const svg = document.createElement("svg");
+  svg.innerHTML = `
+  <svg class="app__section-task-icon-status" width="24" height="24" viewBox= 0 0 24 24" fill="none"
+    cmlns="http://www.w3,org/2000/svg">
+    <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
+    <path d="m9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+      fill="#01080E"></path>
+  </svg>
+  `;
+
+  const paragrafo = document.createElement('p');
+  paragrafo.textContent = tarefa.descricao;
+  paragrafo.classList.add("app__section-task-list-item-description")
+
+  const botao = document.createElement('button');
+  const imagemBotao = document.createElement('img');
+  imagemBotao.setAttribute('src', '/imagens/edit.png');
+  botao.append(imagemBotao);
+
+  li.append(svg);
+  li.append(paragrafo);
+  li.append(botao);
+
+  return li;
+}
 
 btnAdcionarTarefa.addEventListener("click", () => {
   formAdicionarTarefa.classList.toggle("hidden");
 });
 
-formAdicionarTarefa.addEventListener("submit", (evento) => {
+btnAddTask.addEventListener("click", () => {
+  formAddTask.classList.toggle("hidden");
+});
+
+formAdicionarTarefa.addEventListener("submit", function (evento) {
   evento.preventDefault();
+  const textarea = document.querySelector(".app__form-textarea");
   const tarefa = {
     descricao: textarea.value,
   };
+  let tarefas = [];
   tarefas.push(tarefa);
-  localStorage.setItem("tarefas", tarefas);
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+});
+
+tarefas.forEach((tarefa) => {
+  const elementoTarefa = criarElementoTarefa(tarefa);
+  ulTarefas.append(elementoTarefa);
 });
